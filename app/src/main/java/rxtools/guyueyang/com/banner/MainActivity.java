e988a0d01f613259ebd8da3d0256a320bd05dcf4
guyueyang <82650979@qq.com>
2018-04-18 14:39:22 +0800

    123

--- app/src/main/java/rxtools/guyueyang/com/banner/MainActivity.java
+++ app/src/main/java/rxtools/guyueyang/com/banner/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Banner banner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        banner=(Banner)findViewById(R.id.banner);
+//        banner.setImages(
     }
 }
